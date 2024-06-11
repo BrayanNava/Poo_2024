e88df7cec88043bc888ed0f675eb0ed39354b039
BrayanNava <brayan_3141230088@utd.edu.mx>
2024-06-11 08:32:58 -0600

    Commit Final del Parcial 1 of VSCode

--- Parcial1/12_Manejo_Errores/Errores.py
+++ Parcial1/12_Manejo_Errores/Errores.py
@@ -2,27 +2,27 @@
 
 #ejemplo se precenta un error cuando no es jenerada una vareable 
 
-# try:
-#     nombre=input("Dame el nombre completo de una persoa: ")
+try:
+    nombre=input("Dame el nombre completo de una persoa: ")
 
-#     if len(nombre)>0:
-#         nombre_usuario = "El nombre del usuari del sistema es: "+nombre
+    if len(nombre)>0:
+        nombre_usuario = "El nombre del usuari del sistema es: "+nombre
 
-#     print(nombre_usuario)
-# except:
-#     print("Es nesesario el nombre de una persona")
+    print(nombre_usuario)
+except:
+    print("Es nesesario el nombre de una persona")
 
 #Ejemplo 2 cuando se solisita un numero y se ingresa otra cosa
 
-# try:
-#     numero=int(input("ingresa un numero entero: "))
+try:
+    numero=int(input("ingresa un numero entero: "))
 
-#     if numero > 0:
-#         print("Soy un numero entero positivo")
-#     else:
-#         print("Soy un numero entero negativo")
-# except ValueError:
-#     print("No es posible una letra a un numero, verific por favor")
+    if numero > 0:
+        print("Soy un numero entero positivo")
+    else:
+        print("Soy un numero entero negativo")
+except ValueError:
+    print("No es posible una letra a un numero, verific por favor")
 
 #Ejemplo 3 Cuando se generan multiples exepciones
 
@@ -38,4 +38,3 @@ else:
     print("No se genero ningun error")
 finally:
     print("ya termino")
-    
